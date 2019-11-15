7087ab07a53b6ca380d7e679ccd55514aaa0a0c0
Milorad Cvjetkovic <milorad.cvjetkovic@arm.com>
2019-11-15 14:37:06 +0100

    Updated location of SockServer for PC application, build script and related documentation

---
+++ Tools/SockServer/PC/Win/Build.bat
@@ -0,0 +1,3 @@
+cd Source
+mingw32-gcc SockServer.c -l ws2_32 -I ..\Include -o ..\SockServer.exe
+cd ..
--- Tools/SockServer/PC/Win/Build/Build_exe.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-md Exe
-cd Source
-mingw32-gcc SockServer.c -l ws2_32 -I ..\Include -o ..\Exe\SockServer.exe
-cd Build
